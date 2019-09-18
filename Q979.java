52480e7344f1c213635e585ee222f2a852273854
Shawn Yang <yangshuo960502@foxmail.com>
2019-09-19 07:42:22 +0800

    finish Q979

--- Q979.java
+++ Q979.java
@@ -2,7 +2,7 @@
 * @Author: Shawn Yang
 * @Date:   2019-09-11 14:05:33
 * @Last Modified by:   Shawn Yang
-* @Last Modified time: 2019-09-11 14:05:42
+* @Last Modified time: 2019-09-17 19:13:35
 */
 /**
  * Definition for a binary tree node.
@@ -30,5 +30,33 @@ class Solution {
         int right = dfs(root.right);
         ans += Math.abs(left) + Math.abs(right);
         return root.val + left + right - 1;
+    }
+}
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int ans;
+    public int distributeCoins(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        dfs(root);
+        return ans;
+    }
+    public int dfs(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        ans += Math.abs(left) + Math.abs(right);
+        return left + right + root.val - 1;
     }
 }
